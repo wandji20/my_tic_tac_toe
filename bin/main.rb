8cb08b4b5f7ac6c9c2eb1dfeb7f563376792dc6a
wandji20 <wbertrand10@gmail.com>
2020-12-10 15:13:15 +0100

    remove puts from board class

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,52 @@
-#!/usr/bin/envy ruby
+#!/usr/bin/env ruby
+require_relative '../lib/board.rb'
+require_relative '../lib/player.rb'
+require_relative '../lib/game.rb'
+
+puts 'Welcome to tic tac toe'
+puts ''
+puts 'Please input name for player 1'
+name1 = gets.chomp
+player1 = Player.new(name1, 'X')
+while player1.valid_name(name1) == false
+  puts 'Invalid Name!!!'
+  name1 = gets.strip
+end
+player1.name = name1
+puts ''
+puts 'Please input name for player 2'
+name2 = gets.chomp
+player2 = Player.new(name2, 'O')
+while player2.valid_name(name1) == false || name1 == name2
+  puts 'Invalid Name!!!'
+  name2 = gets.strip
+end
+player2.name = name2
+puts ''
+puts "#{name1} starts!"
+
+my_game = Game.new(player1, player2)
+my_board = Board.new
+
+i = 0
+while i < 9
+  player = i.even? ? player1 : player2
+  puts "#{player.name}! Pick a number between 1 and 9 inclusive "
+  num = gets.chomp.to_i
+  player = i.even? ? player1 : player2
+  next unless my_game.chosen(num)
+
+  my_avatar = my_game.avatar(player)
+  my_board.update_board(my_avatar, num - 1)
+  puts my_board.display_board
+  # update player array
+  player.update_my_num(num)
+  # check winner
+  if my_game.game_result(player)
+    puts "#{player.name} is the winner "
+    break
+  end
+
+  i += 1
+end
+p 'DRAW' unless my_game.game_result(player)
--- lib/board.rb
+++ lib/board.rb
@@ -1,15 +1,15 @@
 class Board
   attr_reader :board
   def initialize()
-    @board = %w[1 2 3 4 5 6 7 8 9]
+    @board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
   end
 
   def display_board
-    puts " #{board[0]} | #{board[1]} | #{board[2]} "
-    puts '-----------'
-    puts " #{board[3]} | #{board[4]} | #{board[5]} "
-    puts '-----------'
-    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  [  " #{board[0]} | #{board[1]} | #{board[2]} ",
+    '-----------',
+    " #{board[3]} | #{board[4]} | #{board[5]} ",
+    '-----------',
+    " #{board[6]} | #{board[7]} | #{board[8]} "]
   end
 
   def update_board(avatar, index)
